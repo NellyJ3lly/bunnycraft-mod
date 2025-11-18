b496e7706f20af5c9193ae5e9817dd9fe147a0a0
NellyJ3lly <IcyAb07@gmail.com>
2025-11-18 14:01:48 -0700

    I think I did this awhile ago but I modified the loot tables of stuff

--- src/main/java/net/bunnycraft/modifiers/ModifyLootTables.java
+++ src/main/java/net/bunnycraft/modifiers/ModifyLootTables.java
@@ -19,22 +19,22 @@ public class ModifyLootTables {
     public static void modifyLootTables(){
         LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
             if (source.isBuiltin()) {
-                if(LootTables.ANCIENT_CITY_CHEST == key){
-                    LootPool.Builder poolBuilder = LootPool.builder()
+                if(LootTables.ANCIENT_CITY_CHEST.equals(key)){
+                    LootPool.Builder smithingTemplatePoolBuilder = LootPool.builder()
                             .rolls(ConstantLootNumberProvider.create(1))
                             .conditionally(RandomChanceLootCondition.builder(0.1f))
                             .with(ItemEntry.builder(ModItems.STEEL_UPGRADE_TEMPLATE))
                             .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f,1.0f)).build());
-                    tableBuilder.pool(poolBuilder);
-                    poolBuilder.build();
+                    tableBuilder.pool(smithingTemplatePoolBuilder);
+                    smithingTemplatePoolBuilder.build();
                 }
-                if(LootTables.SNIFFER_DIGGING_GAMEPLAY == key){
-                    LootPool.Builder smithingTemplatePoolBuilder = LootPool.builder()
+                if(LootTables.SNIFFER_DIGGING_GAMEPLAY.equals(key)){
+                    LootPool.Builder sniffer_digging_builder = LootPool.builder()
                             .rolls(ConstantLootNumberProvider.create(1))
                             .conditionally(RandomChanceLootCondition.builder(0.25f))
                             .with(LootTableEntry.builder(ModSimpleLootTableProvider.ADDITIONAL_SNIFFER_LOOT).build());
-                    tableBuilder.pool(smithingTemplatePoolBuilder);
-                    smithingTemplatePoolBuilder.build();
+                    sniffer_digging_builder.build();
+                    tableBuilder.pool(sniffer_digging_builder);
                 }
             }
         });
